24aecc99f01db359a733176089c4ea3253819a7d
kerubo <mongaremetrine5@gmail.com>
2026-04-14 09:07:45 +0300

    more models

--- server/models.py
+++ server/models.py
@@ -20,3 +20,47 @@ class Exercise(db.Model):
         if len(value) < 3:
             raise ValueError("Exercise name too short")
         return value
+    
+    # Workout model
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default=date.today)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='workout', cascade='all, delete')
+
+    @validates('duration_minutes')
+    def validate_duration(self, key, value):
+        if value <= 0:
+            raise ValueError("Duration must be positive")
+        return value
+
+    
+    # Workout  Exercise model
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    workout = db.relationship('Workout', back_populates='workout_exercises')
+    exercise = db.relationship('Exercise', back_populates='workout_exercises')
+
+    __table_args__ = (
+        db.UniqueConstraint('workout_id', 'exercise_id', name='unique_workout_exercise'),
+    )
+
+    @validates('reps', 'sets', 'duration_seconds')
+    def validate_numbers(self, key, value):
+        if value is not None and value <= 0:
+            raise ValueError("Must be positive")
+        return value
